9eba5a877d820742a4358d517a730e1d77b1d80d
AdamGabris <adamga@student.uia.no>
2024-02-09 01:49:23 +0100

    Initial commit, implemented conversion functions. Missing tests.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Converter
+{
+    int expected;
+    int actual;
+
+    public static double ConvertToMm(double inches)
+    {
+        return inches * 25.4;
+    }
+    public static double ConvertToCm(double inches)
+    {
+        return inches * 2.54;
+    }
+    public static double ConvertToM(double inches)
+    {
+        return inches * 0.0254;
+    }
+    public void Test()
+    {
+        if (expected == actual)
+        {
+            Console.WriteLine("Success");
+        }
+        else
+        {
+            Console.WriteLine("Failure");
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var item in args)
+        {
+            if (item.Contains("mm"))
+            {
+                Console.WriteLine(Converter.ConvertToMm);
+            }
+            else if (item.Contains("cm"))
+            {
+                Console.WriteLine(Converter.ConvertToCm);
+
+            }
+            else if (item.Contains("m"))
+            {
+                Console.WriteLine(Converter.ConvertToM);
+
+            }
+        }
+    }
+}
+
+
